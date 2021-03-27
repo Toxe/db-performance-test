4d72e8089eb05b82a70537b46954d0caccc5e8ad
Toxe <toxe@toxe.net>
2021-03-27 17:18:52 +0100

    Use spdlog and fmt

--- src/db_insert.cpp
+++ src/db_insert.cpp
@@ -1,7 +1,9 @@
-#include "clipp.h"
 #include <filesystem>
 #include <iostream>
 #include <tuple>
+
+#include <clipp.h>
+#include <spdlog/spdlog.h>
 
 void show_usage_and_exit(const clipp::group& cli, const char* argv0, const char* error_message = nullptr)
 {
@@ -20,16 +22,23 @@ auto eval_args(int argc, char* argv[])
     bool run_multi = false;
     bool run_all = true;
     bool show_help = false;
+    auto log_level = spdlog::level::warn;
 
     auto cli = (
         (clipp::option("--single").set(run_single).set(run_all, false).doc("run single inserts for every row"),
          clipp::option("--multi").set(run_multi).set(run_all, false).doc("insert multiple rows in one request")) |
         clipp::option("--all").set(run_all).doc("run all tests (default)"),
-        clipp::option("-h", "--help").set(show_help).doc("show help")
+        clipp::option("-h", "--help").set(show_help).doc("show help"),
+        clipp::option("-v", "--verbose").set(log_level, spdlog::level::info).doc("show verbose output")
     );
 
     if (!clipp::parse(argc, argv, cli))
         show_usage_and_exit(cli, argv[0]);
+
+    spdlog::set_level(log_level);
+    spdlog::info("CLI option --single: {}", run_single);
+    spdlog::info("CLI option --multi: {}", run_multi);
+    spdlog::info("CLI option --all: {}", run_all);
 
     if (run_all) {
         run_single = true;
@@ -46,6 +55,6 @@ int main(int argc, char* argv[])
 {
     auto [run_single, run_multi] = eval_args(argc, argv);
 
-    std::cout << run_single << '\n';
-    std::cout << run_multi << '\n';
+    spdlog::info("run single test: {}", run_single);
+    spdlog::info("run multi test: {}", run_multi);
 }

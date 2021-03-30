4f193b579193cb8749128d84f37c025dbe59da5c
Toxe <toxe@toxe.net>
2021-03-31 01:39:06 +0200

    Define combined_logger pattern and level

--- src/combined_logger.cpp
+++ src/combined_logger.cpp
@@ -8,13 +8,12 @@
 std::shared_ptr<spdlog::logger> create_combined_logger(const std::string& logfile_name)
 {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
-    console_sink->set_level(spdlog::level::info);
-
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_st>(logfile_name, false);
-    file_sink->set_level(spdlog::level::info);
-
     std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
+
     auto combined_logger = std::make_shared<spdlog::logger>("combined", sinks.begin(), sinks.end());
+    combined_logger->set_level(spdlog::level::info);
+    combined_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
 
     spdlog::register_logger(combined_logger);
 

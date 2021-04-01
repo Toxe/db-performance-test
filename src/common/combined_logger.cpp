fad89497bde3b0fcc6d0d973e14417606efd8fa2
Toxe <toxe@toxe.net>
2021-04-01 14:41:31 +0200

    Flush logger every 60 seconds

--- src/common/combined_logger.cpp
+++ src/common/combined_logger.cpp
@@ -1,14 +1,17 @@
 #include "combined_logger.h"
 
+#include <chrono>
 #include <vector>
 
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
+using namespace std::chrono_literals;
+
 std::shared_ptr<spdlog::logger> create_combined_logger(const std::string& logfile_name)
 {
-    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_st>();
-    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_st>(logfile_name, false);
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logfile_name, false);
     std::vector<spdlog::sink_ptr> sinks{console_sink, file_sink};
 
     auto combined_logger = std::make_shared<spdlog::logger>("combined", sinks.begin(), sinks.end());
@@ -16,6 +19,7 @@ std::shared_ptr<spdlog::logger> create_combined_logger(const std::string& logfil
     combined_logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] %v");
 
     spdlog::register_logger(combined_logger);
+    spdlog::flush_every(60s);
 
     return combined_logger;
 }
